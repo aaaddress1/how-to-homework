f53042b7cd1226b2cc318386874525166b062b0f
馬聖豪 <aaaddress1@adr-pc.local>
2016-12-05 01:09:21 +0800

    no message

---
+++ 404.c/404.c
@@ -0,0 +1,8 @@
+/**
+*	display 404
+*	from stack overflow: what's the joke in the stack overflow 404 page code?
+*	http://meta.stackoverflow.com/questions/252184/whats-the-joke-in-the-stack-overflow-404-page-code
+*/
+#include <stdio.h>
+#define/*404*/v/*put*/putchar
+int main() { return v(4+v(v(52)-4))-52; }
